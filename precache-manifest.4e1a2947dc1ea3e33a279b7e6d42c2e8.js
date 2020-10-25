62f35617edbce1bdc9fe02ca2503d31f8bcd7ce2
Ridoy Rajoan <rezuenlos@gmail.com>
2020-10-25 18:03:34 +0600

    Updates

---
+++ precache-manifest.4e1a2947dc1ea3e33a279b7e6d42c2e8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7458afdbf8720f04b67e6b030097d18",
+    "url": "/de-landing/index.html"
+  },
+  {
+    "revision": "a6f2b0b7c563d71d4ab6",
+    "url": "/de-landing/static/css/main.390c8f4c.chunk.css"
+  },
+  {
+    "revision": "ae304b202b9cd9091993",
+    "url": "/de-landing/static/js/2.c2b9534f.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/de-landing/static/js/2.c2b9534f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6f2b0b7c563d71d4ab6",
+    "url": "/de-landing/static/js/main.a81c8e11.chunk.js"
+  },
+  {
+    "revision": "73a2d714c973ae49a805",
+    "url": "/de-landing/static/js/runtime-main.f43582f1.js"
+  },
+  {
+    "revision": "c9f8a9a89572aa8aba93d6508f05da8a",
+    "url": "/de-landing/static/media/dhaka-eats-logo.c9f8a9a8.png"
+  },
+  {
+    "revision": "98bfe3e4e2bfa71cdc9ffe9b4c685967",
+    "url": "/de-landing/static/media/mobilescreen.98bfe3e4.png"
+  },
+  {
+    "revision": "a256f4dfcc3f28a7424f8f9daad68cf7",
+    "url": "/de-landing/static/media/screens2.a256f4df.png"
+  }
+]);
